0103cec61a212e1eab3ae585d5a5779bd61b1807
Nicolae Pop <nicupop729@gmail.com>
2021-11-17 16:03:46 -0300

    implement interaction with API to send and receive data, implement the display on UI

--- src/display-comments.js
+++ src/display-comments.js
@@ -1,51 +1,125 @@
 // eslint-disable-next-line import/no-cycle
 import { getRecipe } from './hitApi';
 
-const buildPopUp = async (e) => {
-  const object = await getRecipe(e.target.id);
+const domManip = (object) => {
   const htmlText = `
-    <div class="pop-up-comment-div">
-      <div class="inner-pop-up-comment-div">
-        <h3 class="recipe-title">${object.title}</h3>
-        <div class="grid">
-          <div class="pop-up-img-div">
-            <img
-              class="pop-up-img"
-              src="${object.image_url}"
-            />
-          </div>
-          <p class="servings">Servings: ${object.servings}</p>
-          <p class="recipe">See full recipe <a class="link" href="${object.source_url}" target="_blank" rel="noopener">here</a></p>
-          <p class="inspiration">Inspired by: ${object.publisher}</p>          
-          <p class="ingredients">Ingredients</p>
-          <ul class="ingredients-list">
-          </ul>
+  <div class="pop-up-comment-div">
+    <div class="inner-pop-up-comment-div">
+      <h3 class="recipe-title">${object.title}</h3>
+      <div class="grid">
+        <div class="pop-up-img-div">
+          <img
+            class="pop-up-img"
+            src="${object.image_url}"
+          />
         </div>
+        <p class="servings">Servings: ${object.servings}</p>
+        <p class="recipe">See full recipe <a class="link" href="${object.source_url}" target="_blank" rel="noopener">here</a></p>
+        <p class="inspiration">Inspired by: ${object.publisher}</p>          
+        <p class="ingredients">Ingredients</p>
+        <ul class="ingredients-list">
+        </ul>
       </div>
-      <span class="close-modul"><ion-icon name="close-outline"></ion-icon></span>
+      <div class="comments-section">
+        <h3 class="comments-title">Comments (45)</h3>
+        <ul class="comments-list">          
+        </ul>
+        <p class="leave-comm-text">Leave your comment</p>
+        <form class="leave-comm">
+          <input type="text" class="input-name" placeholder="Your name" required>
+          <textarea type="text" class="input-mess" placeholder="Your message" required></textarea>
+          <button class="pop-up-btn" type="submit">Send your message</button>
+        </form>
+      </div>
     </div>
-      `;
+    <span class="close-modul"><ion-icon name="close-outline"></ion-icon></span>
+  </div>
+    `;
 
   document.body.insertAdjacentHTML('afterbegin', htmlText);
+};
 
-  const ingredientsList = document.querySelector('.ingredients-list');
-  const closeIcon = document.querySelector('.close-modul');
-  const popUpDiv = document.querySelector('.pop-up-comment-div');
-
-  // popUpDiv.style.display = 'block';
-
-  object.ingredients.forEach((ingr) => {
+const displayIngredients = (ingredients, container) => {
+  ingredients.forEach((ingr) => {
     const textHTML = `
     <li class="ingredient">${ingr.quantity === null ? '' : ingr.quantity} ${
   ingr.unit === 'tsp' ? 'teaspoons' : ingr.unit
 } ${ingr.unit === 'tsp' || ingr.unit === 'cup' ? 'of' : ''} ${
   ingr.description
 }</li>`;
-    ingredientsList.insertAdjacentHTML('beforeend', textHTML);
+    container.insertAdjacentHTML('beforeend', textHTML);
   });
-  closeIcon.addEventListener('click', () => {
-    popUpDiv.style.display = 'none';
+};
+
+const closePopUp = (x, popUp) => {
+  x.addEventListener('click', () => {
+    popUp.style.display = 'none';
   });
+};
+
+const getComm = async (pizzaId) => {
+  const dataComm = await fetch(
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8FcrK9POw5EbfAJUs4DD/comments?item_id=${pizzaId}`,
+  );
+  const comm = await dataComm.json();
+  return comm;
+};
+
+const sendNewComm = (pizzaId, name, comment) => {
+  fetch(
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/8FcrK9POw5EbfAJUs4DD/comments',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: pizzaId,
+        username: name,
+        comment,
+      }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    },
+  );
+};
+
+const getCommArray = async (pizzaId, container) => {
+  const commArr = await getComm(pizzaId);
+  commArr.forEach((comm) => {
+    const textHTML = `<li class="comment">${comm.creation_date} <span class="user-name">${comm.username}</span>: ${comm.comment}</li>`;
+    container.insertAdjacentHTML('afterbegin', textHTML);
+  });
+};
+
+const buildPopUp = async (e) => {
+  const pizzaId = e.target.id;
+  const apiResult = await getRecipe(pizzaId);
+
+  domManip(apiResult);
+
+  const ingredientsList = document.querySelector('.ingredients-list');
+  const closeIcon = document.querySelector('.close-modul');
+  const popUpDiv = document.querySelector('.pop-up-comment-div');
+  const form = document.querySelector('.leave-comm');
+  const inputName = document.querySelector('.input-name');
+  const inputMess = document.querySelector('.input-mess');
+  const commentsList = document.querySelector('.comments-list');
+
+  popUpDiv.style.display = 'block';
+
+  displayIngredients(apiResult.ingredients, ingredientsList);
+
+  getCommArray(pizzaId, commentsList);
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    sendNewComm(pizzaId, inputName.value, inputMess.value);
+    inputName.value = '';
+    inputMess.value = '';
+    setTimeout(() => {
+      commentsList.innerHTML = '';
+      getCommArray(pizzaId, commentsList);
+    }, 2000);
+  });
+
+  closePopUp(closeIcon, popUpDiv);
 };
 
 const displayPopUp = (btns) => {
