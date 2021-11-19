8c8e7c95f406ecfc4b592239eedc6dfe13363dd7
micheaol <micheaol80@gmail.com>
2021-11-19 09:05:50 +0100

    add logic to update likes without refreshing the whole page

---
+++ src/displayLike.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable import/extensions */
+import { getLikes } from './hitApi';
+
+const showLiked = async (likedData, dat, namePizz) => {
+  likedData = [];
+  await getLikes().then((theLike) =>
+    theLike.forEach((datLike) => {
+      likedData.push(datLike);
+    })
+  );
+  likedData.forEach((lik) => {
+    if (lik.item_id === dat.id) {
+      if (lik.likes > 1) {
+        namePizz.textContent = `${lik.likes} Likes`;
+      }
+      if (lik.likes === 1) {
+        namePizz.textContent = `${lik.likes} Like`;
+      }
+    }
+  });
+};
+
+export default showLiked;
